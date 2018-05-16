e080bb608c64463359fdca6a96e1ba3dce6c3db5
luca590 <lucasgaylord@protonmail.com>
2018-05-15 22:40:23 -0400

    adding uncommitted changes to tracked files

--- simpleVoterContract.sol
+++ simpleVoterContract.sol
@@ -1,31 +1,32 @@
 //https://medium.com/@mvmurthy/full-stack-hello-world-voting-ethereum-dapp-tutorial-part-1-40d2d0d807c2
+// npm install solc - solidity compiler
 pragma solidity ^0.4.11; //need to specify version of compiler to be called, potential security risk if wrong
 
 contract Voting {	
 
 	// Equivalent to AssociativeArray or Hash. Key-value mapping. Key is bytes32 adn value is uint8
-	mapping (bytes32 ==> uint8) public votesRecieved		//will store the number of votes recieved
+	mapping (bytes32 => uint8) public votesRecieved;		//will store the number of votes recieved
 	bytes32[] public candidateList;	//array of candidates (strings), stored as an array of 32 bytes 
 	
-	function Voting(bytes32[] candidateNames) { // This is our constructor for the class
+	function Voting(bytes32[] candidateNames) public { // This is our constructor for the class
 //constructor gets called only once when dapp is deployed on Eth. Cannot overwrite, can only create new contract which will be initialized with its own constructor
 			candidateList = candidateNames;
 	}
 	
 	//Returns total votes candidate has recieved so far
-	function totalVotesFor(bytes32 candidate) return(uint) {
-			require(validCandidate(candidate))
-			return votesRecieved[candidate];
+	 function totalVotesFor(bytes32 candidate) view public returns (uint8) {
+		     require(validCandidate(candidate));
+		     return votesReceived[candidate];
 	}
-	
+
 	//increments vote count for specific candidate
-	function votingForCandidate(bytes32 candidate) {
-			require(validCandidate(candidate))	//same as if(false) throw
-			votesRecieved[candidate] += 1
+	function votingForCandidate(bytes32 candidate) public {
+			require(validCandidate(candidate));	//same as if(false) throw
+			votesRecieved[candidate] += 1;
 	}
 	
 	//checks validity of candidate
-	function validCandidate(bytes32 candidate) return(bool) {
+	function validCandidate(bytes32 candidate) view public returns (bool) {
 			for (uint i = 0; i < candidateList.length; i++) {
 					if(candidateList[i] == candidate) {
 							return true;
